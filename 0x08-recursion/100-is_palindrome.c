6d41503af1e1f84d2737d8e0bc6fa7b2744a1ff5
bharaniyhao <adzikahyao@gmail.com>
2023-06-07 10:54:47 +0000

    up100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,19 +8,23 @@
  *
  * Return: 1 if @s is a palindrome, 0 otherwise.
  */
+int is_palindrome_recursive(char *s, int start, int end)
+{
+    if (start >= end)
+        return 1;
+
+    if (s[start] != s[end])
+        return 0;
+
+    return is_palindrome_recursive(s, start + 1, end - 1);
+}
+
 int is_palindrome(char *s)
 {
-	int len = strlen(s);
-	int i;
+    int len = strlen(s);
 
-	if (len <= 1)
-		return 1;
+    if (len <= 1)
+        return 1;
 
-	for (i = 0; i < len / 2; i++)
-	{
-		if (s[i] != s[len - i - 1])
-			return 0;
-	}
-
-	return 1;
+    return is_palindrome_recursive(s, 0, len - 1);
 }
