f0934db5f9bf66f8b1af3ef50a8101969ea0a918
bharaniyhao <adzikahyao@gmail.com>
2023-06-07 10:36:32 +0000

    update6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,21 +11,22 @@
 int is_prime_number_recursive(int n, int i);
 
 /**
- * is_prime_number - function that returns 1 if the input integer is a prime number, otherwise return 0
+ * is_prime_number - function that returns 1 if the input integer is a prime number,
+ *                   otherwise return 0
  * @n: input
  *
  * Return: 1 if @n is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	else if (n <= 3)
-		return (1);
-	else if (n % 2 == 0 || n % 3 == 0)
-		return (0);
-	else
-		return (is_prime_number_recursive(n, 5));
+    if (n <= 1)
+        return 0;
+    else if (n <= 3)
+        return 1;
+    else if (n % 2 == 0 || n % 3 == 0)
+        return 0;
+    else
+        return is_prime_number_recursive(n, 5);
 }
 
 /**
@@ -37,10 +38,10 @@ int is_prime_number(int n)
  */
 int is_prime_number_recursive(int n, int i)
 {
-	if (i * i > n)
-		return (1);
-	else if (n % i == 0 || n % (i + 2) == 0)
-		return (0);
-	else
-		return (is_prime_number_recursive(n, i + 6));
+    if (i * i > n)
+        return 1;
+    else if (n % i == 0 || n % (i + 2) == 0)
+        return 0;
+    else
+        return is_prime_number_recursive(n, i + 6);
 }
