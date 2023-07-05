a1d63d915516e36da8ebddeecfb7853128229869
bharaniyhao <adzikahyao@gmail.com>
2023-07-05 06:41:21 -0700

    update

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -4,22 +4,21 @@
 #include "lists.h"
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
- */
+ *  * main - check the code
+ *   * 
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
     listint_t *head;
     listint_t *new;
     listint_t hello = {8, NULL};
     size_t n;
-
     head = &hello;
     new = malloc(sizeof(listint_t));
     if (new == NULL)
     {
-        printf("Error\n");
+     printf("Error\n");
         return (1);
     }
     new->n = 9;
