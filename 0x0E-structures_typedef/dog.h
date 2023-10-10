eaa808b91acf486d8c73c6a1d4deedaa8a3f0527
Yao Adzikah <adzikahyao@gmail.com>
2023-10-10 10:22:11 +0000

    88932

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,82 +2,81 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - Defines a dog with a name, age, and owner.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * Description: This struct defines a dog's basic information.
+ * Description: This structure represents a dog's basic information.
  */
 typedef struct dog dog_t;
 
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+    char *name;   /* The name of the dog. */
+    float age;    /* The age of the dog. */
+    char *owner;  /* The owner of the dog. */
 };
 
 /**
- * init_dog - initializes a dog structure
- * @d: pointer to the dog structure
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * init_dog - Initializes a dog structure.
+ * @d: A pointer to the dog structure to initialize.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
  * Description: This function initializes a dog structure with the given values.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
- * print_dog - prints a dog structure
- * @d: pointer to the dog structure
+ * print_dog - Prints the details of a dog structure.
+ * @d: A pointer to the dog structure to print.
  *
  * Description: This function prints the details of a dog structure.
  */
 void print_dog(struct dog *d);
 
 /**
- * new_dog - creates a new dog structure
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * new_dog - Creates a new dog structure.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * Return: pointer to the newly created dog structure
+ * Return: A pointer to the newly created dog structure.
  *
  * Description: This function creates a new dog structure and initializes it
- * with the provided values.
+ *              with the provided values.
  */
 dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * free_dog - frees a dog structure
- * @d: pointer to the dog structure
+ * free_dog - Frees the memory allocated for a dog structure.
+ * @d: A pointer to the dog structure to free.
  *
  * Description: This function frees the memory allocated for a dog structure.
  */
 void free_dog(dog_t *d);
 
 /**
- * _strcpy - copies a string
- * @dest: destination string
- * @src: source string
+ * _strcpy - Copies a string.
+ * @dest: The destination string.
+ * @src: The source string.
  *
- * Return: pointer to the destination string
+ * Return: A pointer to the destination string.
  *
  * Description: This function copies the source string to the destination string.
  */
 char *_strcpy(char *dest, char *src);
 
 /**
- * _strlen - calculates the length of a string
- * @s: input string
+ * _strlen - Calculates the length of a string.
+ * @s: The input string.
  *
- * Return: length of the string
+ * Return: The length of the string.
  *
  * Description: This function calculates the length of the input string.
  */
 int _strlen(char *s);
 
 #endif
-
