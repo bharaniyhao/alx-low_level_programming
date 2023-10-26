dfe9b0e5d6ba45461e4a0c465468b144b783e9d6
Yao Adzikah <adzikahyao@gmail.com>
2023-10-26 04:59:29 -0700

    push

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success, the number of characters written.
+ * On error, -1 is returned.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
